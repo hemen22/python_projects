05a1345d27de187b0ba3bb83c24d8ba814e53a55
hemen22 <155385633+hemen22@users.noreply.github.com>
2024-06-25 12:07:57 +0300

    modified README.MD
    import random

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # python_projects
 python practice projects 
+
+## number guessing game
+
+### import random 
+
+The random module offers a variety of functions to generate random data, including:
+> random.random(): Generates a random floating-point number between 0.0 (inclusive) and 1.0 (exclusive).
+> random.randint(a, b): Generates a random integer between a (inclusive) and b (exclusive).
+> random.choice(sequence): Returns a random element from the given sequence (list, string, tuple).
+#### Examples of Use Cases:
+
+Simulating coin flips or dice rolls in games.
+Shuffling elements in a list to randomize order.
+Selecting random samples from a dataset for analysis.
+Creating unpredictable behavior in artificial intelligence or machine learning algorithms.
---
+++ number_guessing.py
@@ -0,0 +1,40 @@
+import random #The random module offers a variety of functions to generate random data
+
+top_of_range = input("type a number:")
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    
+    if top_of_range <= 0:
+        print('please type a number larger than 0 next time.')
+        quit()
+else:
+    print('type a number')
+    quit()        
+
+random_number = random.randint(0,top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("make a guess: ")
+    if user_guess.isdigit():
+      user_guess = int(user_guess)
+    else:
+      print('please type a number only')
+      continue
+
+    if user_guess == random_number:
+       print("you got it ;)")
+       break
+    elif user_guess > random_number:
+          print("your guess were above the number!")
+    else:
+          print("your guess were below the number!")
+
+print("you got it in" , guesses , "Guesses")
+       
+
+
+
+
+
